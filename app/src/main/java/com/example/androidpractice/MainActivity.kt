c291989171fe9d11097891a25a9d91323a2365ca
BolNick <BolNick09RUS@gmail.com>
2026-03-14 22:42:16 +0300

    + BottomNavigationView (кривое)

--- app/src/main/java/com/example/androidpractice/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/MainActivity.kt
@@ -6,14 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 
 import com.example.androidpractice.databinding.ActivityMainBinding
 
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager
+                .findFragmentById(R.id.nav_host) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
